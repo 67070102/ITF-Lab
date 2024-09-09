1969d4e35f6c3e9cf6b1375d3d682adbfd336f15
67070102 <67070102@kmitl.ac.th>
2024-09-09 21:53:13 +0700

    สร้างโดยใช้ Colab

---
+++ Test.py
@@ -0,0 +1,43 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "toc_visible": true,
+      "authorship_tag": "ABX9TyMmAyPq0XEPeCaqroSirZ9Q"
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "Wy4rVmagU_i3",
+        "outputId": "892ddc9b-a9d4-45e9-86ff-ce07717a9a18",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        }
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello world\n"
+          ]
+        }
+      ],
+      "source": [
+        "print(\"hello world\")"
+      ]
+    }
+  ]
+}
